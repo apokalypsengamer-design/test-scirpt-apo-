e3d48fbadc3a344280d68f73c03fdb5f3d8265e9
apokalypsengamer-design <apokalypsengamer@gmail.com>
2026-01-31 15:44:25 +0100

    Create fxmanifest.lua for AntiCheat Test Suite
    Initial setup for AntiCheat Test Suite with NUI Menu.

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Apo'
+description 'AntiCheat Test Suite with NUI Menu'
+version '1.0.0'
+
+-- Client Scripts
+client_scripts {
+    'client.lua'
+}
+
+-- UI Files
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+-- Metadata
+lua54 'yes'
